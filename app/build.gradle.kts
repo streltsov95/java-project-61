fe306a723e580ccd76c70cb77ed44157cc0f95db
Viktor Streltsov <streltsov95va@gmail.com>
2023-11-28 13:00:38 +0300

    add gradle application plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,10 +2,15 @@ plugins {
     id("java")
     id("se.patrikerdes.use-latest-versions") version "0.2.18"
     id("com.github.ben-manes.versions") version "0.50.0"
+    application
 }
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
+
+application {
+    mainClass = "hexlet.code.App"
+}
 
 repositories {
     mavenCentral()
