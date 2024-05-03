f04846286fde4df00e42114222d0cd933c1deecb
Viktor Streltsov <streltsov95va@gmail.com>
2024-05-03 21:21:06 +0300

    added standard input for run command process

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,6 +16,10 @@ application {
     mainClass = "hexlet.code.App"
 }
 
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
+}
+
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
