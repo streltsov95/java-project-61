6c30b39be133776e8b4033374b3d7ec43683b437
Viktor Streltsov <streltsov95va@gmail.com>
2024-05-03 20:07:21 +0300

    add Gradle Use Latest Versions Plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java")
+    id("com.github.ben-manes.versions") version "0.51.0"
+    id("se.patrikerdes.use-latest-versions") version "0.2.18"
 }
 
 group = "hexlet.code"
@@ -10,7 +12,7 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation(platform("org.junit:junit-bom:5.10.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
