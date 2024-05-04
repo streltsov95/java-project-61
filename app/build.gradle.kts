c112eddd9b0046e1c19e9dc3b9cdceff0339cee6
Viktor Streltsov <streltsov95va@gmail.com>
2024-05-04 18:55:59 +0300

    Add Checkstyle config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("com.github.ben-manes.versions") version "0.51.0"
     id("se.patrikerdes.use-latest-versions") version "0.2.18"
     id("application")
+    id("checkstyle")
 }
 
 group = "hexlet.code"
