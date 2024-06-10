49f88fee5f480147a47cd9499964fed2fbf3dc7f
Viktor Streltsov <streltsov95va@gmail.com>
2024-06-10 16:38:00 +0300

    Refactor Prime game

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -1,11 +1,13 @@
 package hexlet.code.games;
 
 import hexlet.code.Engine;
-
-import java.util.Random;
+import hexlet.code.Utils;
 
 public class Prime {
     public static final String RULES = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
+
+    private static final int LEAST_VALUE = 2;
+    private static final int UPPER_BOUND = 101;
 
     public static void runPrimeGame() {
         var roundsQuantity = Engine.ROUNDS_QUANTITY;
@@ -18,7 +20,7 @@ public class Prime {
     }
 
     private static String[] roundsDatasetGenerator() {
-        int number = new Random().nextInt(2, 101);
+        int number = Utils.getRandomNumeric(LEAST_VALUE, UPPER_BOUND);
         String correctAnswer = isPrime(number) ? "yes" : "no";
         String question = String.valueOf(number);
         return new String[]{question, correctAnswer};
