9379619fb46758fb9a4a5550665a36926ff113a3
Viktor Streltsov <streltsov95va@gmail.com>
2024-06-10 16:15:12 +0300

    Refactor GCD game

--- app/src/main/java/hexlet/code/games/GCD.java
+++ app/src/main/java/hexlet/code/games/GCD.java
@@ -1,11 +1,13 @@
 package hexlet.code.games;
 
 import hexlet.code.Engine;
-
-import java.util.Random;
+import hexlet.code.Utils;
 
 public class GCD {
     public static final String RULES = "Find the greatest common divisor of given numbers.";
+
+    private static final int LEAST_VALUE = 1;
+    private static final int UPPER_BOUND = 101;
 
     public static void runGCDGame() {
         var roundsQuantity = Engine.ROUNDS_QUANTITY;
@@ -18,8 +20,8 @@ public class GCD {
     }
 
     private static String[] roundsDatasetGenerator() {
-        var number1 = new Random().nextInt(1, 101);
-        var number2 = new Random().nextInt(1, 101);
+        var number1 = Utils.getRandomNumeric(LEAST_VALUE, UPPER_BOUND);
+        var number2 = Utils.getRandomNumeric(LEAST_VALUE, UPPER_BOUND);
         var question = number1 + " " + number2;
         var correctAnswer = gcd(number1, number2);
         return new String[]{question, String.valueOf(correctAnswer)};
