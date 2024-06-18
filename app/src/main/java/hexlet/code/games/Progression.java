1710c919bd2241fe715db9f2f947eced2fd7abb9
Viktor Streltsov <streltsov95va@gmail.com>
2024-06-18 17:39:28 +0300

    Rename methods and constant

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -8,13 +8,13 @@ import java.util.Random;
 public class Progression {
     public static final String RULES = "What number is missing in the progression?";
 
-    private static final int NUMBER_LEAST_VALUE = 1;
+    private static final int NUMBER_LOWER_BOUND = 1;
     private static final int NUMBER_UPPER_BOUND = 101;
 
-    private static final int PROGRESSION_LENGTH_LEAST_VALUE = 5;
+    private static final int PROGRESSION_LENGTH_LOWER_BOUND = 5;
     private static final int PROGRESSION_LENGTH_UPPER_BOUND = 11;
 
-    private static final int PROGRESSION_STEP_LEAST_VALUE = 2;
+    private static final int PROGRESSION_STEP_LOWER_BOUND = 2;
     private static final int PROGRESSION_STEP_UPPER_BOUND = 16;
 
 
@@ -22,19 +22,19 @@ public class Progression {
         var roundsCount = Engine.ROUNDS_COUNT;
         var gamesDataset = new String[roundsCount][];
         for (var round = 0; round < roundsCount; round++) {
-            gamesDataset[round] = roundsDatasetGenerator();
+            gamesDataset[round] = generateGamesData();
         }
 
         Engine.run(gamesDataset, RULES);
     }
 
-    private static String[] roundsDatasetGenerator() {
-        int firstNumber = Utils.getRandomNumeric(NUMBER_LEAST_VALUE, NUMBER_UPPER_BOUND);
-        int progressionLength = Utils.getRandomNumeric(PROGRESSION_LENGTH_LEAST_VALUE, PROGRESSION_LENGTH_UPPER_BOUND);
-        int progressionStep = Utils.getRandomNumeric(PROGRESSION_STEP_LEAST_VALUE, PROGRESSION_STEP_UPPER_BOUND);
+    private static String[] generateGamesData() {
+        int firstNumber = Utils.getRandomNumeric(NUMBER_LOWER_BOUND, NUMBER_UPPER_BOUND);
+        int progressionLength = Utils.getRandomNumeric(PROGRESSION_LENGTH_LOWER_BOUND, PROGRESSION_LENGTH_UPPER_BOUND);
+        int progressionStep = Utils.getRandomNumeric(PROGRESSION_STEP_LOWER_BOUND, PROGRESSION_STEP_UPPER_BOUND);
         int hiddenNumber = new Random().nextInt(progressionLength);
 
-        String[] progression = progressionGenerator(firstNumber, progressionLength, progressionStep);
+        String[] progression = generateProgression(firstNumber, progressionLength, progressionStep);
         String correctAnswer = progression[hiddenNumber];
         progression[hiddenNumber] = "..";
         String question = String.join(" ", progression);
@@ -42,7 +42,7 @@ public class Progression {
         return new String[]{question, correctAnswer};
     }
 
-    private static String[] progressionGenerator(int firstNumber, int length, int step) {
+    private static String[] generateProgression(int firstNumber, int length, int step) {
         String[] progression = new String[length];
         int accumulator = firstNumber;
         for (var i = 0; i < length; i++) {
