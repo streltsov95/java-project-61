9e3ffa593d2838e38c2a37005954feb4773f9c6e
Viktor Streltsov <streltsov95va@gmail.com>
2024-06-18 17:37:30 +0300

    Rename method and constant

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -6,21 +6,21 @@ import hexlet.code.Utils;
 public class Prime {
     public static final String RULES = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
 
-    private static final int LEAST_VALUE = 2;
+    private static final int LOWER_BOUND = 2;
     private static final int UPPER_BOUND = 101;
 
     public static void run() {
         var roundsCount = Engine.ROUNDS_COUNT;
         var gamesDataset = new String[roundsCount][];
         for (var round = 0; round < roundsCount; round++) {
-            gamesDataset[round] = roundsDatasetGenerator();
+            gamesDataset[round] = generateGamesData();
         }
 
         Engine.run(gamesDataset, RULES);
     }
 
-    private static String[] roundsDatasetGenerator() {
-        int number = Utils.getRandomNumeric(LEAST_VALUE, UPPER_BOUND);
+    private static String[] generateGamesData() {
+        int number = Utils.getRandomNumeric(LOWER_BOUND, UPPER_BOUND);
         String correctAnswer = isPrime(number) ? "yes" : "no";
         String question = String.valueOf(number);
         return new String[]{question, correctAnswer};
